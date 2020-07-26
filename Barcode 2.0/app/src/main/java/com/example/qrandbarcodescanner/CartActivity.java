e092a6f92d39cda086ba7451aa82b24877b31d94
Shafiq Aiman <32039212+ShafiqAiman@users.noreply.github.com>
2020-07-27 04:29:15 +0800

    Barcode 2.0

---
+++ Barcode 2.0/app/src/main/java/com/example/qrandbarcodescanner/CartActivity.java
@@ -0,0 +1,15 @@
+package com.example.qrandbarcodescanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
