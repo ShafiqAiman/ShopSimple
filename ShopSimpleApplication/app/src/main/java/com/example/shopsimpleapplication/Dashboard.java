bab88c8ef8cbeb0e2e0fca6300e03be502ee773d
danks0ju <66083560+danks0ju@users.noreply.github.com>
2020-08-07 03:22:52 +0800

    UI update

--- ShopSimpleApplication/app/src/main/java/com/example/shopsimpleapplication/Dashboard.java
+++ ShopSimpleApplication/app/src/main/java/com/example/shopsimpleapplication/Dashboard.java
@@ -3,6 +3,7 @@ package com.example.shopsimpleapplication;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,9 +24,10 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements View.OnClickListener{
 
     Button callLogOut, verifyBtn, callScan, callCart, callHistory;
+    CardView scanner, cart, receipt, logout;
     TextView verifyText;
     FirebaseAuth fAuth;
 
@@ -39,7 +41,7 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
 
-        callScan = findViewById(R.id.toScan);
+        /*callScan = findViewById(R.id.toScan);
 
         callScan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,12 +76,22 @@ public class Dashboard extends AppCompatActivity {
             }
 
 
-        });
+        });*/
+
+        //define cards
+        scanner = (CardView) findViewById(R.id.toScan);
+        cart = (CardView) findViewById(R.id.toCart);
+        receipt = (CardView) findViewById(R.id.toReceipts);
+
+        scanner.setOnClickListener(this);
+        cart.setOnClickListener(this);
+        receipt.setOnClickListener(this);
 
 
-        callLogOut = findViewById(R.id.logoutBtn);
 
-        callLogOut.setOnClickListener(new View.OnClickListener() {
+        logout = (CardView)findViewById(R.id.toLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
@@ -124,6 +136,22 @@ public class Dashboard extends AppCompatActivity {
             });
 
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent i;
+
+        switch (v.getId()){
+
+            case R.id.toScan : i = new Intent(this, ProductDetails.class);startActivity(i);break;
+            case R.id.toCart : i = new Intent(this, CartActivity.class);startActivity(i); break;
+            case R.id.toReceipts : i = new Intent(this, PurchaseHistory.class);startActivity(i); break;
+            default:break;
+
+        }
+
     }
 
         /*public void receiptgen(View v){
